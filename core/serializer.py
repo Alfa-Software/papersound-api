0bdd8704a73bb122ccc73d60d7e02baa7032308a
Julio <jn5.guape@gmail.com>
2020-12-12 18:57:10 -0300

    views e serializaer da nova tabela

--- core/serializer.py
+++ core/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Post, Comentario, Curtida,Curtida_comentario, Seguidor
+from .models import Post, Comentario, Curtida,Curtida_comentario, Seguidor, Conversa, Mensagem
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
@@ -37,3 +37,13 @@ class ListaseguidosSerializer(serializers.ModelSerializer):
     class Meta:
         model=Seguidor
         fields = ['user']
+
+class ConversaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Conversa
+        fields='__all__'
+
+class MensagemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Mensagem
+        fields='__all__'
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, generics
-from .models import Post, Comentario, Curtida,Curtida_comentario, Seguidor
+from .models import Post, Comentario, Curtida,Curtida_comentario, Seguidor, Conversa, Mensagem
 from .serializer import PostSerializer, ComentarioSerializer, CurtidaSerializer, Curtida_comentarioSerializer, SeguidorSerializer, ListaseguidoresSerializer, ListaseguidosSerializer
+from .serializer import MensagemSerializer, ConversaSerializer
+from user.models import User
 
 
 class SeguidoresViewSet(viewsets.ModelViewSet):
@@ -10,7 +12,7 @@ class SeguidoresViewSet(viewsets.ModelViewSet):
     serializer_class = SeguidorSerializer
 
 class PostsViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
+    queryset = Post.objects.all().order_by('-datetime')
     page_size = 10
     serializer_class = PostSerializer
 
@@ -42,3 +44,51 @@ class Listaseguidos(generics.ListAPIView):
         queryset = Seguidor.objects.filter(seguid_id=self.kwargs['pk'])
         return queryset
     serializer_class = ListaseguidosSerializer
+
+class  Listacomentariopost(generics.ListAPIView):
+    """Listando comentarios de um post"""
+    def get_queryset(self):
+        queryset = Comentario.objects.order_by('-datetime').filter(post_id=self.kwargs['pk'])    
+        return queryset
+    serializer_class = ComentarioSerializer
+
+class  Listaposts(generics.ListAPIView):
+    """Listando posts do usuario"""
+    def get_queryset(self):
+        queryset = Post.objects.order_by('-datetime').filter(postador_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = PostSerializer
+
+class Listaseguidoresnome(generics.ListAPIView):
+    """listando seguidores do usuario"""
+    def get_queryset(self):
+        user = User.objects.filter(username=self.kwargs['username']).get()
+        queryset = Seguidor.objects.filter(user_id=self.user.id)
+        return queryset
+    serializer_class = ListaseguidoresSerializer
+
+class Listaseguidosnome(generics.ListAPIView):
+    """listando pessoas que o usuario ta seguindo"""
+    def get_queryset(self):
+        user = User.objects.filter(username=self.kwargs['username']).get()
+        queryset = Seguidor.objects.filter(seguid_id=self.id)
+        return queryset
+    serializer_class = ListaseguidosSerializer
+
+class  Listapostsnome(generics.ListAPIView):
+    """Listando posts do usuario"""
+    def get_queryset(self):
+        user = User.objects.filter(username=self.kwargs['username']).get()
+        queryset = Post.objects.order_by('-datetime').filter(postador_id=user.id)
+        return queryset
+    serializer_class = PostSerializer
+
+class ConversaViewSet(viewsets.ModelViewSet):
+    queryset = Mensagem.objects.all()
+    page_size = 10
+    serializer_class = ConversaSerializer
+
+class MensagemViewSet(viewsets.ModelViewSet):
+    queryset = Conversa.objects.all()
+    page_size = 10
+    serializer_class = MensagemSerializer
